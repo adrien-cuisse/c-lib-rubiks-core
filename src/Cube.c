81dabc4f2fdc3b71291637b37616c00d3301fd5d
Adrien Cuisse <adrien.cuisse@epitech.eu>
2024-10-07 15:17:23 -0400

    made Face an inner class of Cube (encapsulation), corresponding ADR

--- src/Cube.c
+++ src/Cube.c
@@ -6,6 +6,18 @@
 #include "Face.h"
 
 
+/** The 3 horizontal slices composing the face (Y axis) */
+#define TOP_SLICE 0
+#define MIDDLE_SLICE 1
+#define BOTTOM_SLICE 2
+
+
+/** The position of a cell in a slice (X axis) */
+#define LEFT 0
+#define CENTER 1
+#define RIGHT 2
+
+
 /** The position of each face in the T-shaped pattern of the unfolded cube */
 #define LEFT 0
 #define FRONT 1
@@ -17,12 +29,54 @@
 
 
 
+struct Face
+{
+	Color cells[3][3];
+};
+
+
 struct Cube
 {
 	Face * faces[6];
 };
 
 
+
+
+static Face * createFace(Color color);
+
+
+static void deleteFace(Face ** this);
+
+
+static Color getColor(Face const * this);
+
+
+static Color getTopLeftCell(Face const * this);
+
+
+static Color getTopCenterCell(Face const * this);
+
+
+static Color getTopRightCell(Face const * this);
+
+
+static Color getMiddleLeftCell(Face const * this);
+
+
+static Color getMiddleCenterCell(Face const * this);
+
+
+static Color getMiddleRightCell(Face const * this);
+
+
+static Color getBottomLeftCell(Face const * this);
+
+
+static Color getBottomCenterCell(Face const * this);
+
+
+static Color getBottomRightCell(Face const * this);
 
 
 static void createAndPositionFaces(Cube * this);
@@ -80,6 +134,110 @@ static void rotateCameraClockwise(Cube * this);
 static void rotateCameraAnticlockwise(Cube * this);
 
 
+
+
+static void paintSlice(Face * const this, int sliceIndex, Color color)
+{
+	int cellIndex;
+
+	for (cellIndex = LEFT; cellIndex <= RIGHT; cellIndex++)
+			this->cells[sliceIndex][cellIndex] = color;
+}
+
+
+static void paintSlices(Face * const this, Color color)
+{
+	int sliceIndex;
+
+	for (sliceIndex = TOP_SLICE; sliceIndex <= BOTTOM_SLICE; sliceIndex++)
+		paintSlice(this, sliceIndex, color);
+}
+
+
+static Face * createFace(Color color)
+{
+	Face * this = calloc(1, sizeof(* this));
+	if (this == NULL)
+	{
+		fputs("Face allocation failed", stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	paintSlices(this, color);
+
+	return this;
+}
+
+
+static void deleteFace(Face ** this)
+{
+	if ((this == NULL) || (* this == NULL))
+		return;
+
+	free(* this);
+	* this = NULL;
+}
+
+
+static Color getColor(Face const * const this)
+{
+	/* center cells are fixed */
+	return getMiddleCenterCell(this);
+}
+
+
+static Color getTopLeftCell(Face const * const this)
+{
+	return this->cells[TOP_SLICE][LEFT];
+}
+
+
+static Color getTopCenterCell(Face const * const this)
+{
+	return this->cells[TOP_SLICE][CENTER];
+}
+
+
+static Color getTopRightCell(Face const * const this)
+{
+	return this->cells[TOP_SLICE][RIGHT];
+}
+
+
+static Color getMiddleLeftCell(Face const * const this)
+{
+	return this->cells[MIDDLE_SLICE][LEFT];
+}
+
+
+static Color getMiddleCenterCell(Face const * const this)
+{
+	return this->cells[MIDDLE_SLICE][CENTER];
+}
+
+
+static Color getMiddleRightCell(Face const * const this)
+{
+	return this->cells[MIDDLE_SLICE][RIGHT];
+}
+
+
+static Color getBottomLeftCell(Face const * const this)
+{
+	return this->cells[BOTTOM_SLICE][LEFT];
+}
+
+
+static Color getBottomCenterCell(Face const * const this)
+{
+	return this->cells[BOTTOM_SLICE][CENTER];
+}
+
+
+static Color getBottomRightCell(Face const * const this)
+{
+	return this->cells[BOTTOM_SLICE][RIGHT];
+}
 
 
 static void createAndPositionFaces(Cube * this)
@@ -216,7 +374,25 @@ static void rotateCameraAnticlockwise(Cube * const this)
 
 
 
-static CubeMethods methods =
+static FaceMethods faceMethods =
+{
+	createFace,
+	deleteFace,
+	getColor,
+	getTopLeftCell,
+	getTopCenterCell,
+	getTopRightCell,
+	getMiddleLeftCell,
+	getMiddleCenterCell,
+	getMiddleRightCell,
+	getBottomLeftCell,
+	getBottomCenterCell,
+	getBottomRightCell
+};
+FaceMethods const * const _Face = & faceMethods;
+
+
+static CubeMethods cubeMethods =
 {
 	createCube,
 	deleteCube,
@@ -235,4 +411,4 @@ static CubeMethods methods =
 	rotateCameraClockwise,
 	rotateCameraAnticlockwise
 };
-CubeMethods const * const _Cube = & methods;
+CubeMethods const * const _Cube = & cubeMethods;
