f5053830b4abe37ac3b852dfaf81a92231e92a73
Adrien Cuisse <adrien.cuisse@epitech.eu>
2025-06-05 01:37:24 +0200

    custom asserts doc

--- tests/src/asserts.h
+++ tests/src/asserts.h
@@ -6,8 +6,23 @@
 
 
 
-
-#define assert_faceRotatedClockwise(a,b,c) _assert_faceRotatedClockwise((a), (b), (c), __FILE__, __LINE__)
+/**
+ * Asserts that the [face] has been rotated clockwise
+ *
+ * @param faceBeforeRotation - the 2D array of cells, before rotation
+ *
+ * @param face - the actual face object
+ *
+ * @param faceLabel - either "top", "front", "bottom", "back", "left" or "right"
+ *
+ */
+#define assert_faceRotatedClockwise(faceBeforeRotation, face, faceLabel) \
+	_assert_faceRotatedClockwise( \
+		(faceBeforeRotation), \
+		(face), \
+		(faceLabel), \
+		__FILE__, \
+		__LINE__)
 void _assert_faceRotatedClockwise(
 	Color before[FACE_SIZE][FACE_SIZE],
 	Face const * face,
@@ -16,7 +31,22 @@ void _assert_faceRotatedClockwise(
 	int line);
 
 
-#define assert_faceRotatedAnticlockwise(a,b,c) _assert_faceRotatedAnticlockwise((a), (b), (c), __FILE__, __LINE__)
+/**
+ * Asserts that the [face] has been rotated anticlockwise
+ *
+ * @param faceBeforeRotation - the 2D array of cells, before rotation
+ *
+ * @param face - the actual face object
+ *
+ * @param faceLabel - either "top", "front", "bottom", "back", "left" or "right"
+ */
+#define assert_faceRotatedAnticlockwise(faceBeforeRotation, face, faceLabel) \
+	_assert_faceRotatedAnticlockwise( \
+		(faceBeforeRotation), \
+		(face), \
+		(faceLabel), \
+		__FILE__, \
+		__LINE__)
 void _assert_faceRotatedAnticlockwise(
 	Color before[FACE_SIZE][FACE_SIZE],
 	Face const * face,
@@ -25,7 +55,22 @@ void _assert_faceRotatedAnticlockwise(
 	int line);
 
 
-#define assert_faceFlipped(a,b,c) _assert_faceFlipped((a), (b), (c), __FILE__, __LINE__)
+/**
+ * Asserts that the [face] has been flipped (ie., rotated twice)
+ *
+ * @param faceBeforeRotation - the 2D array of cells, before rotation
+ *
+ * @param face - the actual face object
+ *
+ * @param faceLabel - either "top", "front", "bottom", "back", "left" or "right"
+ */
+#define assert_faceFlipped(faceBeforeRotation, face, faceLabel) \
+	_assert_faceFlipped( \
+		(faceBeforeRotation), \
+		(face), \
+		(faceLabel), \
+		__FILE__, \
+		__LINE__)
 void _assert_faceFlipped(
 	Color before[FACE_SIZE][FACE_SIZE],
 	Face const * face,
